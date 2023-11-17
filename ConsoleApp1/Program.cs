b2e80b1b10b8b070e5d8873f636948302d1cc5a8
akademiyaTOP <kosinovDE@C7A204-5NEW>
2023-11-17 08:51:01 +0300

    Добавил 2 строки вывода текста

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorld
         static void Main()
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет, Мир");
+            Console.WriteLine("Hallo World!");
 
         }
     
