7dd04ca09dc0efa3f392c0db288805d44b60c645
kosinovDE <kosinovDE@C7A204-5NEW>
2023-11-17 08:44:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    
+    }
+
+}
